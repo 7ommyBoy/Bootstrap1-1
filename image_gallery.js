50652308c1f46a8271ad157f0a205dc93acb398d
Tommy Works <93413471+7ommyBoy@users.noreply.github.com>
2022-11-27 23:38:48 -0800

    Update image_gallery.js

--- image_gallery.js
+++ image_gallery.js
@@ -1,16 +1,22 @@
-$(document).ready(function () { //ready() event method
+ //ready() event method
+$(document).ready(function () {
+//click event a
 
-    $("#image_list a").click(function (e) { //click event a
+    $("#image_list a").click(function (e) { 
     
     e.preventDefault();
+//gets href
     
-    var imgUrl = $(this).attr("href"); //gets href
+    var imgUrl = $(this).attr("href"); 
     
     var title_desc = $(this).attr("title");
+//changes the title of the image
+
     
-    $('#caption').html(title_desc); //changes the title of the image
+    $('#caption').html(title_desc); 
+//changes the href of image
     
-    $("#image").attr("src", imgUrl) //changes the href of image
+    $("#image").attr("src", imgUrl) 
     
     });
     
